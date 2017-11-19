f94bf584ecb40cd73aa8ea47f29739ab471a2cd5
Andcircle <32606085+Andcircle@users.noreply.github.com>
2017-11-18 19:53:49 -0500

    Add files via upload

---
+++ code/unwarp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 22:52:27 2017
+
+@author: Trucker
+"""
+import cv2
+
+def unwarp(img, src, dst):
+    h,w = img.shape[:2]
+    # use cv2.getPerspectiveTransform() to get M, the transform matrix, and Minv, the inverse
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    # use cv2.warpPerspective() to warp your image to a top-down view
+    warped = cv2.warpPerspective(img, M, (w,h), flags=cv2.INTER_LINEAR)
+    return warped, M, Minv
